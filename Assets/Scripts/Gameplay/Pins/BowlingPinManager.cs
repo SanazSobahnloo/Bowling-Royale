29314e6d063b6867240b04fc06c27fef205016fe
zahra <habiballahi>
2025-01-20 15:35:08 +0330

    Add the first level assets and prefab the pins and add camera

--- Assets/Scripts/Gameplay/Pins/BowlingPinManager.cs
+++ Assets/Scripts/Gameplay/Pins/BowlingPinManager.cs
@@ -1,18 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BowlingPinManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject pinPrefab;
+    [SerializeField] Vector3 startPosition = new Vector3(0, 0, 10);
+    [SerializeField] float pinSpacing = 0.2f;
+
     void Start()
     {
-        
+        SetupPins();
     }
 
-    // Update is called once per frame
-    void Update()
+    void SetupPins()
     {
-        
+        int totalRows = 4;
+        int pinCount = 0;
+        for (int row = 0; row < totalRows; row++)
+        {
+            for (int column = 0; column <= row; column++)
+
+            {
+                if (pinCount <= 10)
+                {
+                    Vector3 position = startPosition + new Vector3(column * pinSpacing - (row * pinSpacing / 2), 0,  row * pinSpacing);
+                    Instantiate(pinPrefab, position, pinPrefab.transform.rotation);
+                    pinCount++;
+                }
+            }
+
+        }
+
     }
+
+   
 }
