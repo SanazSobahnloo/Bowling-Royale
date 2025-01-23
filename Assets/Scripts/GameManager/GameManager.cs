a4da8d7b501371abecb4eba58c3c6193f78f6243
zahra <habiballahi>
2025-01-23 22:48:30 +0330

    Add a Cinemachine Virtual Camera and change Ground model

---
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+   
+    //{
+        //    Instance = this;
+    //}
+
+    public void GameOver() 
+    {
+    
+    
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
