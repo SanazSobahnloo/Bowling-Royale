577b5a9fd8c87364b828a5a08094a903dc9ae6a8
SanazSobhanloo <cnzsg1@gmail.com>
2025-01-26 01:01:25 +0330

    Link Buttons to scenes

--- Assets/Scripts/GameManager/LevelManager.cs
+++ Assets/Scripts/GameManager/LevelManager.cs
@@ -33,4 +33,8 @@ public class LevelManager : MonoBehaviour
     {
         SceneManager.LoadScene("MainMenu");
     }
+    public void LoadLevels()
+    {
+        SceneManager.LoadScene("Levels");
+    }
 }
